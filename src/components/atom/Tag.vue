effb5f5e646455e063a9a2acd7800443f52f4da6
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-08 23:27:39 +0900

    UPDATE: Add Tag.vue and colors in scss

---
+++ src/components/atom/Tag.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="tag" :class="color" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Image',
+  props: {
+    color: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/common';
+
+.tag {
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+
+  @each $name, $color in $colors {
+    &.#{$name} {
+      background-color: $color;
+    }
+  }
+}
+</style>
