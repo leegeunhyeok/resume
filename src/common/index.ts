9c4dbb4aaf2a38ff86610d9f5d8185cd548ae644
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-10 14:57:28 +0900

    ADD: common modules

---
+++ src/common/index.ts
@@ -0,0 +1,4 @@
+import pkg from '../../package.json';
+
+export const VERSION = pkg.version;
+export const RELEASE_DATE = '2020-10-10';
---
+++ src/common/util.ts
@@ -0,0 +1,10 @@
+export const numberPadding = (target: number, length: number) => {
+  const strNumber = target.toString();
+  if (strNumber.length >= length) {
+    return strNumber;
+  } else {
+    let padding = '';
+    for (let i = 0; i < strNumber.length - length; i++, padding += '0');
+    return padding + strNumber;
+  }
+};
