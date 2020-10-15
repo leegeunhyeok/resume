b5049a67ab21f532d30d720c2e1e6b7b0ad73ffb
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-15 12:18:28 +0900

    ADD: Image component (WIP)

---
+++ src/components/atom/Image.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="image">
+    <img :src="source" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Image',
+  props: {
+    source: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles';
+
+.image {
+  border-radius: $radius;
+  overflow: hidden;
+}
+</style>
