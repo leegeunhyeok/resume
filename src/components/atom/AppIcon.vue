7650d0d40dbcb7fdf01fffd04e5a569505d1f3b6
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-17 10:58:48 +0900

    ADD: AppIcon component with Icon images

---
+++ src/components/atom/AppIcon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app-icon">
+    <img :src="icon" :alt="name"/>
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/common';
+
+$icon-size: 7rem;
+
+.app-icon {
+  cursor: pointer;
+  border-radius: $radius;
+  overflow: hidden;
+  margin: 1rem;
+
+  & > img {
+    width: $icon-size;
+    height: $icon-size;
+  }
+
+  & > p {
+    text-align: center;
+    margin: 0;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #fff;
+    text-shadow: 0 1px 2px rgba(0, 0, 0, .3);
+  }
+}
+</style>
