712311a840b4c23cd46f564ba478881f3b135645
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-17 11:49:43 +0900

    UPDATE: Vue config (HTML title - webpack)

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/dist'
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/dist',
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].title = 'Dev | Geundung';
+        return args;
+      });
+  },
 };
