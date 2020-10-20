89afda31fa9f1ae20de63e25cf873f295840e1cc
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-21 01:26:29 +0900

    ADD: Home page logic to Desktop template

---
+++ src/components/template/Desktop.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="desktop">
+    <div class="desktop__apps">
+      <Icon
+        v-for="(app, i) in apps"
+        :name="app.name"
+        :icon="app.icon"
+        :key="i"
+        @click="appExecute(app)"
+      />
+    </div>
+    <transition name="fade">
+      <Window :title="activeApp?.name" @close="activeApp = null" v-show="activeApp">
+        <template v-slot:side v-if="activeApp?.type === 'list'">
+          <ItemGroup
+            v-for="(list, i) in listGroup"
+            :label="list.group"
+            :items="list.items"
+            :key="i"
+          />
+        </template>
+        <template v-slot:default>
+          <Text size="large" content="Title" bold />
+        </template>
+      </Window>
+    </transition>
+    <Dock />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref, computed } from 'vue';
+import Text from '@/components/atom/Text.vue';
+import Icon from '@/components/atom/Icon.vue';
+import Window from '@/components/organism/Window.vue';
+import Dock from '@/components/organism/Dock.vue';
+import ItemGroup from '@/components/organism/ItemGroup.vue';
+import { AppType, Content, ListItem, DockMenu } from '@/types';
+
+interface DesktopProps {
+  apps: AppType[];
+  dock: DockMenu;
+}
+
+interface ListGroup {
+  group?: string;
+  items: (ListItem & { content: Content[] })[];
+}
+
+export default defineComponent({
+  name: 'Desktop',
+  components: { Text, Icon, Window, ItemGroup, Dock },
+  props: {
+    apps: {
+      type: Array as PropType<AppType[]>,
+      default: [],
+    },
+    dock: {
+      type: Object as PropType<DockMenu>,
+      required: true,
+    },
+  },
+  setup() {
+    const activeApp = ref<null | AppType>(null);
+    const listGroup = computed(() => {
+      if (activeApp.value?.type !== 'list') return [];
+      return activeApp.value.items.reduce((prev, { group, tag, title, content }) => {
+        const groupObject = prev.find(x => x.group === group);
+        groupObject
+          ? groupObject.items.push({ tag, title, content })
+          : prev.push({ group, items: [{ tag, title, content }] });
+        return prev;
+      }, [] as ListGroup[]);
+    });
+
+    const appExecute = (app: AppType) => {
+      if (app.type === 'link') {
+        window.open(app.url, '_blank');
+      } else {
+        activeApp.value = app;
+      }
+    };
+
+    return { appExecute, activeApp, listGroup };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/common';
+
+.desktop {
+  @include page;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__apps {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    padding: 1rem;
+    padding-bottom: $dock-height;
+    max-width: 24rem;
+
+    @include size(sm) {
+      max-width: 40rem;
+    }
+  }
+}
+</style>
