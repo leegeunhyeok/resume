155a4d02c055158d3f5b856579ae1cd6369aa29c
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-21 01:26:49 +0900

    UPDATE: Home page (Based on Desktop template)

--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -1,61 +1,108 @@
 <template>
-  <div class="home">
-    <div class="home__apps">
-      <AppIcon name="Projects" :icon="require('@/assets/app/folder.png')" />
-      <AppIcon name="Activity" :icon="require('@/assets/app/calendar.png')" />
-      <AppIcon name="About me" :icon="require('@/assets/app/note.png')" />
-      <AppIcon name="Terminal" :icon="require('@/assets/app/terminal.png')" />
-      <AppIcon name="Browser" :icon="require('@/assets/app/safari.png')" />
-      <AppIcon name="Email" :icon="require('@/assets/app/message.png')" />
-      <AppIcon name="Contact" :icon="require('@/assets/app/contact.png')" />
-      <AppIcon name="Information" :icon="require('@/assets/app/setting.png')" />
-    </div>
-    <Window title="hello" style="display: none">
-      <template v-slot:side>
-        <ItemGroup label="Languages" :items="items" />
-      </template>
-      <template v-slot:default>
-        <Text size="large" content="Title" bold />
-      </template>
-    </Window>
-    <Dock />
-  </div>
+  <Desktop :apps="Template.apps" :dock="Template.dock" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from '@/store';
-import Text from '@/components/atom/Text.vue';
-import AppIcon from '@/components/atom/AppIcon.vue';
-import Window from '@/components/organism/Window.vue';
-import Dock from '@/components/organism/Dock.vue';
-import ItemGroup from '@/components/organism/ItemGroup.vue';
+import Desktop from '@/components/template/Desktop.vue';
 import { GetterTypes } from '@/store/getter';
 import { useRouter } from 'vue-router';
+import { AppType, DockMenu } from '@/types';
+
+interface DesktopTemplate {
+  apps: AppType[];
+  dock: DockMenu;
+}
+
+const Template: DesktopTemplate = {
+  apps: [
+    {
+      type: 'list',
+      name: 'Projects',
+      icon: require('@/assets/app/folder.png'),
+      items: [
+        {
+          group: 'Laugnage',
+          title: 'JavaScript',
+          tag: 'js',
+          content: [],
+        },
+        {
+          group: 'Laugnage',
+          title: 'TypeScript',
+          tag: 'ts',
+          content: [],
+        },
+        {
+          group: 'Other',
+          title: 'Docker',
+          tag: 'docker',
+          content: [],
+        },
+      ],
+    },
+    {
+      type: 'link',
+      url: '',
+      name: 'Activity',
+      icon: require('@/assets/app/calendar.png'),
+    },
+    {
+      type: 'link',
+      url: '',
+      name: 'About me',
+      icon: require('@/assets/app/note.png'),
+    },
+    {
+      type: 'link',
+      url: '',
+      name: 'Terminal',
+      icon: require('@/assets/app/terminal.png'),
+    },
+    {
+      type: 'link',
+      url: '',
+      name: 'Browser',
+      icon: require('@/assets/app/safari.png'),
+    },
+    {
+      type: 'link',
+      url: 'mailto:dev.ghlee@gmail.com',
+      name: 'Email',
+      icon: require('@/assets/app/message.png'),
+    },
+    {
+      type: 'link',
+      url: '',
+      name: 'Contact',
+      icon: require('@/assets/app/contact.png'),
+    },
+    {
+      type: 'link',
+      url: '',
+      name: 'Information',
+      icon: require('@/assets/app/setting.png'),
+    },
+  ],
+  dock: {
+    memo: {
+      title: '빌드 노트',
+      content: 'Updated: 2020.10.21',
+    },
+    hobby: ['프로그래밍', '운동'],
+    photo: require('@/assets/avatar.png'),
+  },
+};
 
 export default defineComponent({
   name: 'Home',
-  components: { Text, AppIcon, Window, ItemGroup, Dock },
+  components: { Desktop },
   setup() {
     const router = useRouter();
     const { getters } = useStore();
     getters[GetterTypes.READY] || router.push({ path: '/' });
-
-    const items = [
-      {
-        tag: 'js',
-        content: 'Javascript',
-      },
-      {
-        tag: 'ts',
-        content: 'Typescript',
-      },
-      {
-        content: 'None',
-      },
-    ];
-
-    return { items };
+    return { Template };
   },
 });
 </script>
@@ -65,24 +112,5 @@ export default defineComponent({
 
 .home {
   @include page;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  &__apps {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    padding: 1rem;
-    padding-bottom: $dock-height;
-    max-width: 24rem;
-
-    @include size(sm) {
-      max-width: 40rem;
-    }
-  }
 }
 </style>
