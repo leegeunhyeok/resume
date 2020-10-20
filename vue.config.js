d4bd42eb61fca151583ea7a233d8791774456742
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-21 01:55:26 +0900

    UPDATE: Google Analytics setting to webpack conf.

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'development' ? '/' : '/dist',
-  chainWebpack: (config) => {
-    config
-      .plugin('html')
-      .tap((args) => {
-        args[0].title = 'Dev | Geundung';
-        return args;
-      });
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Dev | Geundung';
+      args[0].ga = 'UA-156696575-2';
+      return args;
+    });
   },
 };
