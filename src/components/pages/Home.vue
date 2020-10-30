ef922d8bc566b8fe2ab5295d7a20c705599cb46f
leegeunhyeok <dev.ghlee@gmail.com>
2020-10-31 00:09:37 +0900

    UPDATE: Load data from JSON (WIP)

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <Desktop :apps="Template.apps" :dock="Template.dock" />
+  <Desktop :appData="Template.appData" :dock="Template.dock" />
 </template>
 
 <script lang="ts">
@@ -7,89 +7,15 @@ import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from '@/store';
 import { GetterTypes } from '@/store/getter';
-import { AppType, DockMenu } from '@/types';
 import Desktop from '@/components/templates/Desktop.vue';
 
-interface DesktopTemplate {
-  apps: AppType[];
-  dock: DockMenu;
-}
+// Data
+import ProjectData from '@/data/project.json';
 
-const Template: DesktopTemplate = {
-  apps: [
-    {
-      type: 'list',
-      name: 'Projects',
-      icon: require('@/assets/app/folder.png'),
-      items: [
-        {
-          group: 'Laugnage',
-          title: 'JavaScript',
-          tag: 'js',
-          content: [
-            {
-              type: 'text',
-              data: 'Hello',
-            },
-          ],
-        },
-        {
-          group: 'Laugnage',
-          title: 'TypeScript',
-          tag: 'ts',
-          content: [],
-        },
-        {
-          group: 'Other',
-          title: 'Docker',
-          tag: 'docker',
-          content: [],
-        },
-      ],
-    },
-    {
-      type: 'link',
-      url: '',
-      name: 'Activity',
-      icon: require('@/assets/app/calendar.png'),
-    },
-    {
-      type: 'link',
-      url: '',
-      name: 'About me',
-      icon: require('@/assets/app/note.png'),
-    },
-    {
-      type: 'link',
-      url: '',
-      name: 'Terminal',
-      icon: require('@/assets/app/terminal.png'),
-    },
-    {
-      type: 'link',
-      url: '',
-      name: 'Browser',
-      icon: require('@/assets/app/safari.png'),
-    },
-    {
-      type: 'link',
-      url: 'mailto:dev.ghlee@gmail.com',
-      name: 'Email',
-      icon: require('@/assets/app/message.png'),
-    },
-    {
-      type: 'link',
-      url: '',
-      name: 'Contact',
-      icon: require('@/assets/app/contact.png'),
-    },
-    {
-      type: 'link',
-      url: '',
-      name: 'Information',
-      icon: require('@/assets/app/setting.png'),
-    },
-  ],
+const Template = {
+  appData: {
+    projects: ProjectData,
+  },
   dock: {
     name: 'Geunhyeok LEE',
     hobby: ['프로그래밍', '운동'],
