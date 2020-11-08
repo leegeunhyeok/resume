70ec90bd16f08bd661804d566b4a836e40ade543
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-08 21:31:40 +0900

    UPDATE: AssetListExportPlugin (json -> js for global variable)

--- webpack/assets-export-plugin.js
+++ webpack/assets-export-plugin.js
@@ -2,23 +2,36 @@
 const { RawSource } = require('webpack-sources');
 const fs = require('fs');
 
+const defaultOption = {
+  filename: 'assets.js',
+  ignore: undefined,
+  extension: undefined,
+  absPath: false,
+  pretty: false,
+};
+
 function AssetListExportPlugin(options) {
   this.options = {
-    ...{ filename: 'assets.json', pretty: false },
-    ...(options ? options : null),
+    ...defaultOption,
+    ...options,
   };
 }
 
 AssetListExportPlugin.prototype.apply = function(compiler) {
-  compiler.hooks.emit.tapAsync('Asset List Plugin', (compilation, cb) => {
+  const { filename, ignore, extension, combinePublicPath, pretty } = this.options;
+  const publicPath = compiler.options.output.publicPath || '/';
+
+  compiler.hooks.emit.tapAsync('AssetListExportPlugin', (compilation, cb) => {
     const assets = compilation
       .getAssets()
       .filter(({ source }) => source instanceof RawSource)
-      .map(({ name }) => name);
+      .filter(({ name }) => (ignore ? !name.match(ignore) : true))
+      .filter(({ name }) => (extension ? name.match(extension) : true))
+      .map(({ name }) => (combinePublicPath ? publicPath + name : name));
 
     fs.writeFile(
-      this.options.filename,
-      JSON.stringify(assets, null, this.options.pretty ? 2 : 0),
+      filename,
+      `window.__assets=${JSON.stringify(assets, null, pretty ? 2 : 0)}`,
       err => {
         if (err) {
           throw new Error(err);
