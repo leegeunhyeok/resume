df5fbe4fa784e1e2a5c03b8cf7facbfd7bbf956d
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-12 23:50:23 +0900

    UPDATE: Add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,38 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLine,
+  faLinkedin,
+  faGithub,
+  faGitlab,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(
+  faEnvelope,
+  faGlobeAmericas,
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faLine,
+  faLinkedin,
+  faGithub,
+  faGitlab,
+);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('fa', FontAwesomeIcon)
+  .mount('#app');
