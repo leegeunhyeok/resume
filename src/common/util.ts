e5a675056b65dea76562b395b4265a78680fef71
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-16 14:41:04 +0900

    UPDATE: Add axios, getRepositoryStar util funcion

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,3 +1,10 @@
+import axios from 'axios';
+
+/**
+ * Fill 0 to specified length of number
+ * @param target Target number
+ * @param length Number length (fill 0 to target number)
+ */
 export const numberPadding = (target: number, length: number) => {
   const strNumber = target.toString();
   if (strNumber.length >= length) {
@@ -9,6 +16,11 @@ export const numberPadding = (target: number, length: number) => {
   }
 };
 
+/**
+ * Load image resource (for Caching)
+ * @param imageSources Image source (HTTP GET Path)
+ * @param onProgress Call each loaded
+ */
 export const imagePreload = (imageSources: string[], onProgress?: Function): Promise<void> => {
   const total = imageSources.length;
   let count = 0;
@@ -35,6 +47,11 @@ export const imagePreload = (imageSources: string[], onProgress?: Function): Pro
   ).then(() => {});
 };
 
+/**
+ * Scroll to target position
+ * @param to Position of scroll end
+ * @param done On finished callback function
+ */
 export const scrollTo = (to: number, done: Function) => {
   const element = document.documentElement;
   const perTick = Math.max((to - element.scrollTop) / 10, 5);
@@ -49,20 +66,40 @@ export const scrollTo = (to: number, done: Function) => {
   }, 10);
 };
 
+/**
+ * Make first letter to uppercase
+ * @param value Text
+ */
 export const firstCharToUppercase = (value: string) =>
   value.charAt(0).toUpperCase() + value.replace('_', ' ').slice(1);
 
+/**
+ * Load asset from src/assets
+ * @param path Asset path
+ */
 export const assetFrom = (path: string) => require('@/assets/' + path);
 
+/**
+ * Check user color theme is dark
+ */
 export const isDarkmode = () =>
   !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+/**
+ * Register user theme change event
+ * @param handler Handler function
+ */
 export const watchThemeChange = (handler: Function) => {
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
     handler(!!e.matches);
   });
 };
 
+/**
+ * Open target url page
+ * @param target URL
+ * @param newTab Open as new tab flag (default: true)
+ */
 export const openPage = (target: string, newTab = true) => {
   if (newTab) {
     window.open(target, '_blank');
@@ -70,3 +107,13 @@ export const openPage = (target: string, newTab = true) => {
     location.href = target;
   }
 };
+
+/**
+ * Get target repository's star count
+ * @param username Github username
+ * @param repository Githun repository
+ */
+export const getRepositoryStar = (username: string, repository: string) =>
+  axios
+    .get(`https://api.github.com/repos/${username}/${repository}`)
+    .then(res => res.data['stargazers_count']);
