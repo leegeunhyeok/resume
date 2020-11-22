15da0473aa581add65b21e14944ddcbec1731566
leegeunhyeok <dev.ghlee@gmail.com>
2020-11-22 19:36:55 +0900

    RELEASE: 1.0.1

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,5 +2,5 @@ import pkg from '../../package.json';
 import { version } from 'vue';
 
 export const VERSION = pkg.version;
-export const RELEASE_DATE = '2020-11-21';
+export const RELEASE_DATE = '2020-11-22';
 export const VUE_VERSION = version;
